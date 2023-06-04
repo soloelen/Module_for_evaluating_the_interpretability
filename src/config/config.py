78dd6eed70be30e9d9a62247c84f2e9fc47bd45d
Elena <90508535+soloelen@users.noreply.github.com>
2023-06-04 08:03:37 +0300

    delete config

--- src/config/config.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-NUM_EPOCHS = 4
-BATCH_SIZE = 64
-PATH_TO_DATA = "./data"  # consider ./module as root
-PATH_TO_MODEL = "./models/{}"  # usage: PATH_TO_MODEL.format(model_filename)
